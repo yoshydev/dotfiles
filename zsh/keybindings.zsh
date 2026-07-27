ec5bd5183b743b5b427b505e63700f5039809e16
yoshy <24353896+yoshydev@users.noreply.github.com>
2026-07-27 11:45:13 +0900

    feat: C-uをbackward-kill-lineに変更 (カーソル位置から行頭まで削除)

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -4,3 +4,6 @@
 # history-search-multi-word等のプラグインが^E/^Fをself-insertに上書きするため復元
 bindkey '^E' end-of-line
 bindkey '^F' forward-char
+
+# C-u: 行全体削除(kill-whole-line)ではなく、カーソル位置から行頭まで削除
+bindkey '^U' backward-kill-line
